8c07d6ba056432b1854a52b11a21c2ab23dbb719
Kushal2205a <introvertedspacemine2@gmail.com>
2026-01-26 19:05:12 +0530

    1st commit

--- vault_flutter/lib/main.dart
+++ vault_flutter/lib/main.dart
@@ -3,79 +3,46 @@ import 'serverpod_client.dart';
 
 void main() {
   initServerpodClient();
-  runApp(const MyApp());
+  runApp(const VaultApp());
+
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class VaultApp extends StatelessWidget{
+  const VaultApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Vault',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Vault Home'),
-    );
+    return MaterialApp( title: "Evidence Vault" , home: HomeScreen() ,
+      theme: ThemeData(colorScheme: ColorScheme.dark()),);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({super.key});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  Future<void> _callServer() async {
+  Future<void> _serverCall(BuildContext context) async{
     try {
       final res = await client.greeting.hello('Kushal');
+      final msg = (res as dynamic).message ?? res.toString();
 
-      // Most templates have res.message
-      debugPrint('Server says: ${res.message}');
-    } catch (e) {
-      debugPrint('Server error: $e');
-    }
-  }
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Server : $msg")));
+      }
+    }catch(e){
+      if(context.mounted){
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Server : $e")));}}
 
-
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _callServer,
-        tooltip: 'Call Server',
-        child: const Icon(Icons.cloud),
-      ),
+        appBar: AppBar(title: const Text("Home"), actions: [IconButton(tooltip: 'Ping Backend', onPressed: ()=> _serverCall(context), icon: const Icon(Icons.cloud)), ], )
     );
+    throw UnimplementedError();
   }
 }
+
+
